662cf2adf3f4502658ff3856a64f166f8d1594ed
sevrijss <Seppe.VanRijsselberghe@UGent.be>
2022-07-07 09:37:08 +0200

    Move logic from transformSafely to VirtualStore.ts because that happens for every route. index.ts only supplies functions from Quad => Quad.

--- src/VirtualStore.ts
+++ src/VirtualStore.ts
@@ -1,32 +1,67 @@
-import type {
+import {Quad} from "rdf-js";
+import {
+    BasicRepresentation,
     Conditions,
+    getLoggerFor,
+    INTERNAL_QUADS,
+    PassthroughStore,
     Representation,
+    RepresentationConverter,
     RepresentationPreferences,
     ResourceIdentifier,
-    ResourceStore
+    ResourceStore,
+    transformSafely
 } from "@solid/community-server";
-import {getLoggerFor, PassthroughStore} from "@solid/community-server";
 
+
+const quadPrefs = {type: {'internal/quads': 1}};
 
 export class VirtualStore<T extends ResourceStore = ResourceStore> extends PassthroughStore<T> {
     protected readonly source: T;
+    private topLvlStore: ResourceStore;
+    private readonly converter: RepresentationConverter;
     private readonly logger = getLoggerFor(this);
 
 
     private virtualIdentifiers = {};
 
-    public constructor(source: T) {
+    public setTopStore(topStore: ResourceStore): void {
+        this.topLvlStore = topStore;
+        console.log(this.topLvlStore);
+    }
+
+    public constructor(source: T, topStore: ResourceStore, converter: RepresentationConverter) {
         super(source);
         this.source = source;
+        console.log(topStore);
+        this.converter = converter;
+        this.topLvlStore = topStore;
     }
 
     public addVirtualRoute(name: string,
                            original: ResourceIdentifier,
-                           deriveFunction: (arg0: ResourceIdentifier, arg1: RepresentationPreferences, arg3: Conditions)
-                               => Promise<Representation>): void {
+                           deriveFunction: (arg0: Quad) => Quad | undefined): void {
         // Construct a new function to use the original resource and pass on any preferences and/or conditions
         // @ts-expect-error indexing doesn't work for some reason when using strings
-        this.virtualIdentifiers[name] = (prefs: RepresentationPreferences, cond: Conditions): Promise<Representation> => deriveFunction(original, prefs, cond);
+        this.virtualIdentifiers[name] =
+            async (prefs: RepresentationPreferences, cond: Conditions): Promise<Representation> => {
+                // You will almost certainly never need `then`
+                const result = await this.topLvlStore.getRepresentation(original, quadPrefs, cond);
+
+
+                // Utility function from CSS, will make your life much easier
+                const transformedStream = transformSafely(result.data, {
+                    transform(data: Quad): void {
+                        const result = deriveFunction(data);
+                        if (result) {
+                            this.push(result)
+                        }
+                    },
+                    objectMode: true,
+                });
+                const out = new BasicRepresentation(transformedStream, INTERNAL_QUADS);
+                return await this.converter.handle({representation: out, identifier: original, preferences: prefs});
+            }
     }
 
     // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
@@ -48,7 +83,7 @@ export class VirtualStore<T extends ResourceStore = ResourceStore> extends Passt
             this.logger.info(`[PROCESSING\t${virtualIdentifier}]\tVIRTUAL PATH`);
             // @ts-expect-error The object returns an any type,
             // which the compiler can't work with because we need to return a Promise<Representation>
-            return this.virtualIdentifiers[virtualIdentifier](preferences, conditions)
+            return this.virtualIdentifiers[virtualIdentifier](preferences, conditions);
         }
         return this.source.getRepresentation(identifier, preferences, conditions)
     }
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,38 @@
-import {
-    BasicRepresentation,
-    Conditions,
-    getLoggerFor,
-    INTERNAL_QUADS,
-    Representation,
-    RepresentationConverter,
-    RepresentationMetadata,
-    RepresentationPreferences,
-    ResourceIdentifier,
-    ResourceStore,
-    transformSafely
-} from '@solid/community-server';
 import {VirtualStore} from "./VirtualStore";
 import {Quad} from "rdf-js";
-import N3, {NamedNode, DataFactory} from "n3";
-const { namedNode, literal, defaultGraph, quad } = DataFactory;
+import {DataFactory, NamedNode} from "n3";
+import {ResourceStore} from "@solid/community-server";
+
+const {namedNode, literal, defaultGraph, quad} = DataFactory;
 
 export * from "./VirtualStore";
 
-const quadPrefs = {type: {'internal/quads': 1}};
-
 export class PathBuilder {
-    private readonly logger = getLoggerFor(this);
     private readonly virtualStore: VirtualStore;
-    private readonly converter: RepresentationConverter;
-    private readonly toplvlStore: ResourceStore;
 
-    public constructor(vStore: VirtualStore, converter: RepresentationConverter, toplvlStore: ResourceStore) {
+    public constructor(vStore: VirtualStore, topStore: ResourceStore) {
         this.virtualStore = vStore;
-        this.converter = converter;
-        this.toplvlStore = toplvlStore;
+        this.virtualStore.setTopStore(topStore);
         this.virtualStore.addVirtualRoute('age', {path: 'http://localhost:3000/card.ttl'}, this.getAge);
-        this.virtualStore.addVirtualRoute('name', {path: 'http://localhost:3000/card.ttl'}, this.getName);
     }
 
-    private getAge = async (cardUrl: ResourceIdentifier, prefs: RepresentationPreferences, cond: Conditions): Promise<Representation> => {
-        // You will almost certainly never need `then`
-        const result = await this.toplvlStore.getRepresentation(cardUrl, quadPrefs, cond)
-
-        // Utility function from CSS, will make your life much easier
-        const transformedStream = transformSafely(result.data, {
-            transform(data: Quad): void {
-                if (data.predicate.equals(new NamedNode('http://dbpedia.org/ontology/birthDate'))) {
-                    this.push(quad(
-                        data.subject,
-                        namedNode("https://dbpedia.org/ontology/age"),
-                        literal(yearsPassed(new Date(data.object.value))),
-                        defaultGraph()
-                    ));
-                }
-            },
-            objectMode: true
-        });
-        const out = new BasicRepresentation(transformedStream, INTERNAL_QUADS);
-        return await this.converter.handle({representation: out, identifier: cardUrl, preferences: prefs});
-    };
-
-    private getName = (cardUrl: ResourceIdentifier, prefs: RepresentationPreferences, cond: Conditions) => {
-        console.log(cardUrl);
-        return this.toplvlStore.getRepresentation(cardUrl, prefs, cond);
+    private getAge = (data: Quad): Quad | undefined => {
+        if (data.predicate.equals(new NamedNode('http://dbpedia.org/ontology/birthDate'))) {
+            return quad(
+                data.subject,
+                namedNode("https://dbpedia.org/ontology/age"),
+                literal(yearsPassed(new Date(data.object.value))),
+                defaultGraph()
+            );
+        }
+        return undefined;
     };
 
 }
 
-function yearsPassed(date:Date){
+function yearsPassed(date: Date) {
     const now = new Date().getTime()
     const then = date.getTime();
     const diff = now - then;
-    return Math.floor(diff / (1000*60*60*24*365))
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
 }
